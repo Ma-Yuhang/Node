23fa1b5de346c589e70d6a20c790439ed061f8c1
189690 <2312165827@qq.com>
2023-11-13 09:49:06 +0800

    feat: 处理token

--- 4-express/public/js/index.js
+++ 4-express/public/js/index.js
@@ -14,13 +14,20 @@ loginBtn.onclick = function () {
     credentials: 'include'  // 跨域时允许携带cookie
   }).then(resp => resp.json()).then(res => {
     console.log(res);
+    if (res.token) {
+      sessionStorage.setItem('token', res.token)
+    }
   })
 }
 
 getStuBtn.onclick = function () {
+  const token = sessionStorage.getItem('token') || ''
   fetch('http://127.0.0.1:8080/api/student', {
     method: 'GET',
-    credentials: 'include' // 跨域时允许携带cookie
+    credentials: 'include', // 跨域时允许携带cookie
+    headers: {
+      authorization: `Bearer ${token}`
+    }
   }).then(resp => resp.json()).then(res => {
     console.log(res);
   })
