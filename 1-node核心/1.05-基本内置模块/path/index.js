89c33d7e5c8c7ca2d2189f7715b1768f1a9f4da8
189690 <2312165827@qq.com>
2024-02-27 18:48:24 +0800

    fix: 修改process.cwd

--- 1-node核心/1.05-基本内置模块/path/index.js
+++ 1-node核心/1.05-基本内置模块/path/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 // 得到当前的工作目录
-// console.log(process.cwd()); // c:\Users\189690\Desktop\node
+console.log(process.cwd()); // c:\Users\189690\Desktop\node\1-node核心\1.05-基本内置模块\path
 
 // path.resolve 始终得到一个绝对路径
 // console.log(path.resolve(__filename));  // c:\Users\189690\Desktop\node\1-node核心\1.05-基本内置模块\path\index.js
